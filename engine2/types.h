13260ec88193bd0a298865c6f64d87c94c451a38
Ian Wells <iwells018@gmail.com>
2021-01-19 22:12:02 -0800

    add mouse-related types

--- engine2/types.h
+++ engine2/types.h
@@ -12,6 +12,17 @@ using Callback = std::function<void()>;
 // Event callbacks
 using QuitCallback = std::function<void(const SDL_QuitEvent&)>;
 using KeyboardCallback = std::function<void(const SDL_KeyboardEvent&)>;
+using MouseButtonCallback = std::function<void(const SDL_MouseButtonEvent&)>;
+using MouseWheelCallback = std::function<void(const SDL_MouseWheelEvent&)>;
+using MouseMotionCallback = std::function<void(const SDL_MouseMotionEvent&)>;
+
+enum class MouseButton {
+  kLeft = SDL_BUTTON_LEFT,
+  kMiddle = SDL_BUTTON_MIDDLE,
+  kRight = SDL_BUTTON_RIGHT,
+  kX1 = SDL_BUTTON_X1,
+  kX2 = SDL_BUTTON_X2,
+};
 
 }  // namespace engine2
 
