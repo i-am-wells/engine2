f593635d5bfa5a6fabfdc045e9eaea813f3527c6
Ian Wells <iwells018@gmail.com>
2021-01-31 23:31:55 -0800

    add a few colors

--- engine2/rgba_color.h
+++ engine2/rgba_color.h
@@ -7,13 +7,21 @@
 
 namespace engine2 {
 
-static constexpr uint8_t kOpaque = SDL_ALPHA_OPAQUE;
-static constexpr uint8_t kTransparent = SDL_ALPHA_TRANSPARENT;
+inline constexpr uint8_t kOpaque = SDL_ALPHA_OPAQUE;
+inline constexpr uint8_t kTransparent = SDL_ALPHA_TRANSPARENT;
 
 struct RgbaColor {
   uint8_t r, g, b, a;
   SDL_Color ToSDLColor() const;
 };
+
+// TODO more colors as needed
+inline constexpr RgbaColor kBlack{0, 0, 0, kOpaque};
+inline constexpr RgbaColor kGray{128, 128, 128, kOpaque};
+inline constexpr RgbaColor kDarkGray{64, 64, 64, kOpaque};
+inline constexpr RgbaColor kWhite{255, 255, 255, kOpaque};
+inline constexpr RgbaColor kRed{255, 0, 0, kOpaque};
+inline constexpr RgbaColor kGreen{0, 255, 0, kOpaque};
 
 }  // namespace engine2
 
